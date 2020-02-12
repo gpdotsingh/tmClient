104f2cdba55d2a5ad2a041b2dd65c999a7c2cd50
KLMSampleProject <klmproject.gaurav@gmail.com>
2020-02-13 05:04:02 +0530

    Added cors and added distinct name controller

--- src/main/java/com/ing/client/tm/tmClient/controller/HomeController.java
+++ src/main/java/com/ing/client/tm/tmClient/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.ing.client.tm.tmClient.controller;
 
 import com.ing.client.tm.tmClient.entities.Transaction;
+import com.ing.client.tm.tmClient.entities.UserNames;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
@@ -9,20 +10,43 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.time.LocalDateTime;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@CrossOrigin
 @Controller
 public class HomeController {
 
     @Autowired
     OAuth2RestTemplate oAuth2RestTemplate;
 
-    @RequestMapping(value = "/api/users",method = RequestMethod.GET)
+    @RequestMapping(value = "/client/tm",method = RequestMethod.GET)
     public ResponseEntity<Transaction[]> getTransaction(@RequestParam(value = "startDate") String startDate, @RequestParam(value = "endtDate") String endtDate, @RequestParam(value = "name", required=false) String name, @RequestParam(value = "size", defaultValue = "25",required=false) String size)
     {
-        Transaction[]  userList=   oAuth2RestTemplate.getForEntity("http://localhost:8080/api/tm",
+        String url = "http://localhost:8080/api/tm";
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(url);
+        uriBuilder.queryParam("startDate",startDate);
+        uriBuilder.queryParam("endtDate",endtDate);
+        uriBuilder.queryParam("name",name);
+
+        Transaction[]  userList=   oAuth2RestTemplate.getForEntity(uriBuilder.build().toString(),
                 Transaction[].class).getBody();
         return ResponseEntity.accepted().body(userList);
+    }
+
+    @RequestMapping(method = GET,value= {"/client/name"})
+    public ResponseEntity<List<String>> getDistinctNameList(@RequestParam(value = "nameChar", required=false,defaultValue = "A") String name)
+    {
+        String url = "http://localhost:8080/api/name";
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(url);
+        uriBuilder.queryParam("nameChar",name);
+        List<String> names=   oAuth2RestTemplate.getForEntity(uriBuilder.build().toString(),
+                ArrayList.class).getBody();
+        return ResponseEntity.accepted().body(names);
     }
 }
---
+++ src/main/java/com/ing/client/tm/tmClient/entities/UserNames.java
@@ -0,0 +1,8 @@
+package com.ing.client.tm.tmClient.entities;
+
+import lombok.Data;
+
+@Data
+public class UserNames {
+    private String name;
+}

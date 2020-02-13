2c8f7e8d33c67252b69dafcc2fa3b1111fd64894
KLMSampleProject <klmproject.gaurav@gmail.com>
2020-02-13 23:11:47 +0530

    Comit

--- src/main/java/com/ing/client/tm/tmClient/security/WebSecurityConfiguration.java
+++ src/main/java/com/ing/client/tm/tmClient/security/WebSecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @EnableWebSecurity
@@ -12,10 +11,5 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .mvcMatchers("/**").permitAll()
-                .anyRequest().authenticated();
-
     }
 }

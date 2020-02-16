e513ea37e059d4b6e6c987095d727b001c727bde
KLMSampleProject <klmproject.gaurav@gmail.com>
2020-02-17 01:50:24 +0530

    Include static files

--- src/main/java/com/ing/client/tm/tmClient/security/WebConfiguration.java
+++ src/main/java/com/ing/client/tm/tmClient/security/WebConfiguration.java
@@ -1,22 +1,19 @@
 package com.ing.client.tm.tmClient.security;
 
-import org.springframework.boot.actuate.trace.http.HttpTrace;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("*/**").addResourceLocations("classpath:/static/");
     }
 
     @Bean
